1ef97785e5c8ead4fad7626693a714e4b89dee4e
Sahar Mor <sahar@stripe.com>
2022-06-09 00:32:24 -0700

    config change

--- backend/consts.py
+++ backend/consts.py
@@ -14,7 +14,7 @@ VQGAN_COMMIT_ID = "e93a26e7707683d349bf5d5c41c5b0ef69b677a9"
 GEN_TOP_K = None
 GEN_TOP_P = None
 TEMPERATURE  = None
-COND_SCALE = 3.0
+COND_SCALE = 10.0
 
 class ModelSize(Enum):
     MINI = "Mini"
