8aba752cc565ba0463dc762d39714caa41b5a47f
Sourcery AI <>
2022-06-12 03:57:51 +0000

    'Refactored by Sourcery'

--- backend/dalle_model.py
+++ backend/dalle_model.py
@@ -88,7 +88,7 @@ class DalleModel:
 
         # generate images
         images = []
-        for i in range(max(num_predictions // jax.device_count(), 1)):
+        for _ in range(max(num_predictions // jax.device_count(), 1)):
             # get a new key
             key, subkey = jax.random.split(key)
 
@@ -109,7 +109,9 @@ class DalleModel:
             # decode images
             decoded_images = p_decode(self.vqgan, encoded_images, self.vqgan_params)
             decoded_images = decoded_images.clip(0.0, 1.0).reshape((-1, 256, 256, 3))
-            for img in decoded_images:
-                images.append(Image.fromarray(np.asarray(img * 255, dtype=np.uint8)))
+            images.extend(
+                Image.fromarray(np.asarray(img * 255, dtype=np.uint8))
+                for img in decoded_images
+            )
 
         return images
