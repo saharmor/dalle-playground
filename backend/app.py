e4722bf67d5ce73399eb00671033c6e8cc86c0f2
Sahar Mor <saharhashai@gmail.com>
2022-06-20 22:07:35 -0700

    fix

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ import time
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
-from backend.consts import DEFAULT_IMG_OUTPUT_DIR
+from consts import DEFAULT_IMG_OUTPUT_DIR
 from utils import parse_arg_boolean, parse_arg_dalle_version
 from consts import ModelSize
 
