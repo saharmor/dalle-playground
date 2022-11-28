e17b14bd68be06e3612740b6a4083f78603ab125
Sahar Mor <saharhashai@gmail.com>
2022-11-27 17:27:32 -0800

    Using ImageList over Grid for image gallery

--- interface/src/GeneratedImageList.js
+++ interface/src/GeneratedImageList.js
@@ -1,38 +1,45 @@
 import React from 'react';
-import { Grid } from "@material-ui/core";
 import withStyles from "@material-ui/core/styles/withStyles";
+import ImageList from '@material-ui/core/ImageList';
+import ImageListItem from '@material-ui/core/ImageListItem';
 
 const useStyles = () => ({
+    root: {
+        
+    },
     generatedImg: {
         borderRadius: '8px',
     },
+    imageCell: {
+        margin: "0px 5px 10px 5px",
+    },
 });
 
-const GeneratedImageList = ({ classes, generatedImages, generatedImagesFormat, promptText }) => {
+const GeneratedImageList = ({ classes, generatedImages, generatedImagesFormat, promptText, index }) => {
     const ImageObject = ({ imgData, promptText, index }) => {
         const imgSrc = `data:image/${generatedImagesFormat};base64,${imgData}`
         const alt = `${promptText} ${index}`
-        const title= "Download image"
+        const title = "Download image"
         const downloadedFilename = `${promptText}_${index}.${generatedImagesFormat}`
-        
+
         return (
-            <a href={imgSrc} alt={alt} title={title} download={downloadedFilename}>
-                <img src={imgSrc} className={classes.generatedImg} alt={alt} title={title} />
-            </a>
+            <ImageListItem className={classes.imageCell} key={index}>
+                <a href={imgSrc} alt={alt} title={title} download={downloadedFilename}>
+                    <img src={imgSrc} className={classes.generatedImg} alt={alt} title={title} width="300px"/>
+                </a>
+            </ImageListItem>
         )
     }
 
 
     return (
-        <Grid container alignItems="center" spacing={3}>
-            {generatedImages.map((generatedImg, index) => {
-                return (
-                    <Grid item key={index}>
-                        <ImageObject imgData={generatedImg} promptText={promptText} index={++index}/>
-                    </Grid>
-                )
-            })}
-        </Grid>
+        <div className={classes.root}>
+            <ImageList gap={20} cols={2}>
+                {generatedImages.map((generatedImg, index) => (
+                    <ImageObject imgData={generatedImg} promptText={promptText} key={++index} index={++index}/>
+                ))}
+            </ImageList>
+        </div>
     )
 }
 
